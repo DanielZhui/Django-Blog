1aa3a9fe8217a2ae75954e44a2ff1a924fd73b94
Wollens <zh2017728@163.com>
2019-11-05 23:21:30 +0800

    add article detail

--- Dblog/blog/models.py
+++ Dblog/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 # from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -41,3 +42,6 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
--- Dblog/blog/views.py
+++ Dblog/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
 from . models import Article
 
 # Create your views here.
@@ -11,3 +12,8 @@ def index(request):
     return render(request, 'blog/index.html', context={
         'articles': articles
     })
+
+
+def detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'blog/detail.html', context={'article': article})
