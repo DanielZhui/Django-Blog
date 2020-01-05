2ad92a6232eed0361dd77468f0aff62d6c461c4a
Wollens <zh2017728@163.com>
2020-01-05 13:06:43 +0800

    blog detail show comments

--- Dblog/comments/templatetags/comments_extras.py
+++ Dblog/comments/templatetags/comments_extras.py
@@ -14,3 +14,14 @@ def show_comment_form(context, article, form=None):
         'form': form,
         'article': article
     }
+
+
+@register.inclusion_tag('comments/inclusions/_list.html', takes_context=True)
+def show_comments(context, article):
+    comment_list = article.comments_set.all().order_by('-createdAt')
+    # comment_list = article.objects.filter(article=article)
+    comment_count = comment_list.count()
+    return {
+        'comment_count': comment_count,
+        'comment_list': comment_list
+    }
