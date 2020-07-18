5873f4f97e8f11bfab699a2e8ffc566299fc4a87
Wollens <zh2017728@163.com>
2020-07-18 17:12:59 +0800

    tag count use django annotate

--- Dblog/blog/templatetags/blog_extras.py
+++ Dblog/blog/templatetags/blog_extras.py
@@ -3,8 +3,10 @@
 from django import template
 from django.db import connection
 from django.db.models import Count
+from django.db.models.aggregates import Count
 
 from ..models import Article, Category, Tag
+
 
 register = template.Library()
 
@@ -29,7 +31,6 @@ def show_archives(context):
 
 @register.inclusion_tag('blog/inclusions/_categories.html', takes_context=True)
 def show_categories(context):
-    from django.db.models.aggregates import Count
     category_list = Category.objects.annotate(article_count=Count('article')).filter(article_count__gte=0)
     return {
         'category_list': category_list
@@ -38,15 +39,7 @@ def show_categories(context):
 
 @register.inclusion_tag('blog/inclusions/_tags.html', takes_context=True)
 def show_tags(context):
-    tag_obj_list = []
-    tag_list = Tag.objects.all()
-    for tag in tag_list:
-        tag_obj = {}
-        count = Article.objects.filter(tags=tag).count()
-        tag_obj['name'] = tag.name
-        tag_obj['pk'] = tag.id
-        tag_obj['count'] = count
-        tag_obj_list.append(tag_obj)
+    tags_list = Tag.objects.annotate(article_count=Count('article')).filter(article_count__gt=0)
     return {
-        'tag_obj_list': tag_obj_list
+        'tags_list': tags_list
     }
